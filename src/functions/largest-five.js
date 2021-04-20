3ed1fb5132b42203c33345f8ed8ca4b91c6a39b2
fadi <fadeomar2015@gmail.com>
2021-04-21 00:55:32 +0300

    fix style

--- src/functions/largest-five.js
+++ src/functions/largest-five.js
@@ -1,18 +1,7 @@
 const largestFive = ({ digits }) => {
   //todo
-  console.log({ digits });
-  let fiveDigitsNumbers = [];
-  const digitsArray = digits.split('');
 
-  digitsArray.forEach((number, index) => {
-    if (index + 5 > digitsArray.length) return;
-    const fiveDigitNumber = digitsArray.slice(index, index + 5).join('');
-    fiveDigitsNumbers = [...fiveDigitsNumbers, fiveDigitNumber];
-  });
-
-  const result = Math.max(...fiveDigitsNumbers);
-  console.log({ result });
-  return result;
+  return;
 };
 
 export default largestFive;
